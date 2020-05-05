c1e9e726b2b2b24e5289a6b5514020959f8a5146
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2020-05-04 17:53:55 -0700

    Use load_tidy_delay_data().

--- inst/functions-from-funcs.R
+++ inst/functions-from-funcs.R
@@ -2,45 +2,6 @@
 ##' /CoronaModelsBC/selfIsolationModel/SIR-functionalised/funcs.R, then moving
 ##' one at a time into new files as proper functions. This file is in .Rbuildignore.
 
-##' Load in the detailed linelist data and exclude un-needed columns (that may
-##'  want to be looked at some point) and outliers of negative delays and delays
-##'  >30 days (since likely data-input errors).
-##'  <description>
-##'
-##' @return list of two tibbles with columns `reported_date`, `symptom_onset_date and
-##'   `time_to_report` with each row corresponding to a positive test:
-##'  - delay_data_with_outliers  - with the outliers still included
-##'  - delay_data                - with outliers excluded, to use for analyses
-##' @export
-##' @author Andrew Edwards
-load_tidy_delay_data <- function(){
-  linelist_latest_file <-
-    here::here("nCoVDailyData/linelist/2019-nCoV_daily_linelist.csv")
-  if(!file.exists(linelist_latest_file)){
-    stop(paste("You need to have the file ",
-               linelist_latest_file,
-               " ."))
-  }
-  linelist <- read.csv(linelist_latest_file,
-                     stringsAsFactors = F,
-                     na.strings = "")
-  names(linelist)[1] = "investigation_id"    # else it seems to be "i..investigation_id"
-
-  delay_data_with_outliers = as_tibble(linelist) %>%
-    select(reported_date, symptom_onset_date) %>%
-    mutate_all(lubridate::ymd) %>%
-    filter(!is.na(reported_date) & !is.na(symptom_onset_date)) %>%
-    mutate(time_to_report = reported_date - symptom_onset_date)
-
-  # Removing the following outliers that are $<0$ or $\geq 30$ days, since most
-  #  likely to be data-entry errors, to yield the final dataset.
-  # filter(delay_data_with_outliers,
-  #     time_to_report >= 30 | time_to_report < 0)
-  delay_data <- filter(delay_data_with_outliers,
-                       time_to_report < 30 & time_to_report >= 0)
-  return(list("delay_data" = delay_data,
-              "delay_data_with_outliers" = delay_data_with_outliers))
-}
 
 
 
