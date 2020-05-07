f4fc2086c25cf1ab569e3ea0dd7efa4273718c6f
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2020-05-06 17:39:31 -0700

    Update delay_data and now no need to randomise.

--- data-raw/symptom-to-report.R
+++ data-raw/symptom-to-report.R
@@ -1,18 +1,18 @@
-# Load in the symptoms to reporting data, and save it randomised here since not
-# yet public. This is from report-delay.Rmd. If can make public the true data
-# then can comment out all except first and last lines.
+# Load in the symptoms to reporting data, and save it here.
+#  Had the commented out lines to randomise it (but can now use the raw data).
+#  (Originally from file report-delay.Rmd).
 
 delay_data <- load_tidy_delay_data()[["delay_data"]]
 
 # Add randomness to be able to make public
-add_random_1 <- sample(-3:3, nrow(delay_data), replace=T)
-add_random_2 <- sample(-3:3, nrow(delay_data), replace=T)
+# add_random_1 <- sample(-3:3, nrow(delay_data), replace=T)
+# add_random_2 <- sample(-3:3, nrow(delay_data), replace=T)
 
-delay_data <- delay_data %>%
-  dplyr::mutate(reported_date = reported_date + add_random_1) %>%
-  dplyr::mutate(symptom_onset_date = symptom_onset_date + add_random_2) %>%
-  dplyr::mutate(time_to_report = as.numeric(reported_date - symptom_onset_date)) %>%
-  dplyr::filter(time_to_report >= 0)
+# delay_data <- delay_data %>%
+#   dplyr::mutate(reported_date = reported_date + add_random_1) %>%
+#   dplyr::mutate(symptom_onset_date = symptom_onset_date + add_random_2) %>%
+#   dplyr::mutate(time_to_report = as.numeric(reported_date - symptom_onset_date)) %>%
+#   dplyr::filter(time_to_report >= 0)
 
 usethis::use_data(delay_data,
                   overwrite = TRUE)
