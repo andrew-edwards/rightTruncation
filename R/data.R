812608a7791784c1d0d5f8ad586748e6d53bc32f
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2021-01-11 17:22:57 -0800

    Add code for new data files, saving data as of various dates.

--- R/data.R
+++ R/data.R
@@ -1,7 +1,9 @@
 ##' Raw data from British Columbia on time of symptom onset and time of
 ##' reporting.
 ##'
-##' From the linelist data, see `data-raw/symptom-to-report.R` for details.
+##' From the linelist data, see `data-raw/symptom-to-report.R` for details. This
+##'   was done in June 2020 and has a latest `reported_date` of 2020-06-02. Used
+##'   to explain methods in the vignette.
 ##' @format A tibble with each row being a confirmed case (not all confirmed
 ##'   cases are here, only those with estimated date of symptom onset), and columns:
 ##'   * reported_date - date of case being reported (lubridate date format)
@@ -9,3 +11,17 @@
 ##'   * time_to_report - difference of first two columns, in days
 ##' @source Generated from running `data-raw/symptom-to-report.R`.
 "delay_data"
+
+##' From the linelist data, see `data-raw/symptom-to-report.R` for details.
+##' @format Same as `delay_data`, but done for data uploaded on 23 September 2020
+##'   (so max `reported_date` is 22nd). Due presumably to slight delays in
+##'   amalgamating data, `dplyr::filter(delay_data_2020_09_22, reported_date <=
+##'   "2020-06-02")` has five more rows than `delay_data` (which has
+##'   `max(reported_date)` of 2020-06-02. Hence worth saving a few example data
+##'   sets from various dates.
+"delay_data_2020_09_22"
+
+##' From the linelist data, see `data-raw/symptom-to-report.R` for details.
+##' @format Same as `delay_data` and `delay_data_2020_09_22`, but done for data
+##'   uploaded on 2021-01-11 (with max `reported_date` of 2021-01-05).
+"delay_data_2020_01_05"
--- data-raw/symptom-to-report.R
+++ data-raw/symptom-to-report.R
@@ -1,7 +1,28 @@
 # Load in the symptoms to reporting data, and save it here.
 #  (Originally from file report-delay.Rmd).
 
+# This was done in June 2020 and has max reported_date of 2020-06-02. Keeping as
+#  default since useful for explanations in the vignette.
 delay_data <- load_tidy_delay_data()[["delay_data"]]
 
 usethis::use_data(delay_data,
-                  overwrite = TRUE)
+                  overwrite = FALSE)   # No longer update this
+
+# Doing this, which has a maximum reported date of 2020_09_22, to maybe add to
+#  vignette.
+delay_data_2020_09_22 <- load_tidy_delay_data()[["delay_data"]]
+
+usethis::use_data(delay_data_2020_09_22,
+                  overwrite = FALSE)   # No longer update this
+#  delay_data is not quite the same as
+#   dplyr::filter(delay_data_2020_09_22, reported_date <= "2020-06-02")
+#  probably due to data still tricking in for delay_data. Hence worth saving
+#   these at various times.
+
+
+# Doing this on 2021-01-11, though max reported_date is 2021-01-05, to add to vignette and update
+#  parameter estimates for modelling.
+delay_data_2021_01_05 <- load_tidy_delay_data()[["delay_data"]]
+
+usethis::use_data(delay_data_2021_01_05,
+                  overwrite = FALSE)   # No longer update this
